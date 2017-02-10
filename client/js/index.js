522fe9bf1d7dfb2dc2ca0a5265068bfa60f0923f
Nick Wilson <nwilson9@iastate.edu>
2017-02-09 21:17:39 -0600

    G2G

--- client/js/index.js
+++ client/js/index.js
@@ -1,14 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var SemanticUI = require('semantic-ui-react');
-var test = require('./test.jsx');
+var header = require('./header.jsx');
 
-
-var CHSS = React.createClass({
-    render: function() {
-        return React.createElement(test.testButton);//SemanticUI.Dimmer, {active: true}, React.createElement(SemanticUI.Loader, {active: true}, 'Welcome to CHSS'));
-    }
-});
-
-ReactDOM.render(React.createElement(CHSS), document.getElementById('react-app'));
+ReactDOM.render(React.createElement(header.chssHeader), document.getElementById('react-app'));
 
