42065a445af7a510e6c7562ad69130e45ac2be2b
ajrmatt <ajrmatt@iastate.edu>
2017-03-15 16:25:11 -0500

    Change docs to callback documentation format; match board and piece connectors

--- server/connectors/boardConnector/boardConnector.js
+++ server/connectors/boardConnector/boardConnector.js
@@ -5,99 +5,66 @@
 
 //Link dependencies
 let config = require('./board_connector_config.json');
-const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
-
+const router = require('express').Router();
 
 //Routing
-
-
-/**
- * Helper function for post requests.
- *
- * If no ID is specified in the req body it sends an error to the client.
- * If an ID is specified it queries the server to see if a board with the given ID exists.
- * If so the board is updated, if not then a board is created and stored.
- * Then the resulting updated or new board is sent back to the client.
- * If an error is encountered then the error is sent to the client and execution stops.
- *
- * @param req - contains the information for the request
- * @param res - the object to send results to
- */
-const boardPostHelper = function (req, res){
+router.post('/api/boards', function (req, res) {
     let id = req.body.id;
     if(Object.keys(req.body).length === 0){
-        res.statusCode = 404;
+        res.statusCode = 400;
         res.send("No body specified!");
         return;
     }
     if(!id){
-        res.statusCode = 404;
+        res.statusCode = 400;
         res.send("No id specified!");
         return;
     }
-
     let board = config.boards.find((board) => board.id === id);
     if(board){
         config.boards[config.boards.indexOf(board)] = req.body;
         fs.writeFile(path.join(__dirname, '/board_connector_config.json'), JSON.stringify(config, null, 4), function(err){
-            if(err){
+            if(err) {
                 res.statusCode = 500;
                 res.send('Error while saving board data');
                 console.error(new Date().toLocaleTimeString() + ' : Unable to save board data');
                 console.error(err);
-            }
-            else{
+            } else {
                 res.send(config.boards.find((board) => board.id === id));
             }
         });
     }
-    else{
+    else {
         config.boards[config.boards.length] = req.body;
         fs.writeFile(path.join(__dirname, '/board_connector_config.json'), JSON.stringify(config, null, 4), function(err){
-            if(err){
+            if(err) {
                 res.statusCode = 500;
                 res.send('Error while saving board data');
                 console.error(new Date().toLocaleTimeString() + ' : Unable to save board data');
                 console.error(err);
-            }
-            else{
-                res.send(config.boards[config.boards.length - 1]);
+            } else {
+                res.send(config.boards[config.boards.length-1]);
             }
         });
     }
-};
-router.post('/api/boards', boardPostHelper);
+});
 
-
-/**
- * Helper function for get requests.
- *
- * If IDs are specified in the req body it queries the server to get the information for each board with a specified ID.
- * If one is not found then an error is sent to the requesting client and execution stops.
- * Otherwise all the boards information is sent back to the client.
- * If a creator is specified it queries the server for all boards created by that user.
- * If none are found it sends an error to the client.
- * Otherwise it sends the found boards information to the client.
- *
- * @param req - contains the information for the request
- * @param res - the object to send results to
- */
-const boardGetHelper = function(req, res){
-    let id = req.query.id;
-    if(id){
-        id = id.split(',');
+router.get('/api/boards', function (req, res) {
+    let ids = req.query.id;
+    if(ids) {
+        ids = ids.split(',');
     }
     let creator = req.query.creator;
-    if(!id && !creator){
+    if(!ids && !creator){
         res.statusCode = 404;
-        res.send("No id or creator specified!");
+        res.send("No ids or creator specified!");
         return;
     }
-    if(id && creator){
+    if(ids && creator){
         res.statusCode = 400;
-        res.send("Both id and creator specified");
+        res.send("Both ids and creator specified");
         return;
     }
     let returnBoards = [];
@@ -114,22 +81,53 @@ const boardGetHelper = function(req, res){
         }
         res.send(returnBoards);
     }
-    if(id){
-        config.boards.forEach(function (board){
-            for(var i = 0; i < id.length; i++){
-                if(board.id == id[i]){
+    if(ids){
+        config.boards.forEach(function (board) {
+            for(let i = 0; i < ids.length; i++) {
+                if (board.id == ids[i]) {
                     returnBoards.push(board);
                     return;
                 }
             }
         });
-        if(returnBoards.length === 0 || returnBoards.length != id.length){
+        if(returnBoards.length === 0 || returnBoards.length != ids.length)  {
             res.statusCode = 404;
             res.send("Board id/s not found");
             return;
         }
         res.send(returnBoards);
     }
-};
-router.get('/api/boards', boardGetHelper);
+});
+
 module.exports = router;
+
+/** Documentation **/
+
+/**
+ * Helper function for post requests.
+ *
+ * If no ID is specified in the req body it sends an error to the client.
+ * If an ID is specified it queries the server to see if a board with the given ID exists.
+ * If so the board is updated, if not then a board is created and stored.
+ * Then the resulting updated or new board is sent back to the client.
+ * If an error is encountered then the error is sent to the client and execution stops.
+ *
+ * @callback module:boardConnector~postBoardsCallback
+ * @param req - contains the information for the request
+ * @param res - the object to send results to
+ */
+
+/**
+ * Helper function for get requests.
+ *
+ * If IDs are specified in the req body it queries the server to get the information for each board with a specified ID.
+ * If one is not found then an error is sent to the requesting client and execution stops.
+ * Otherwise all the boards information is sent back to the client.
+ * If a creator is specified it queries the server for all boards created by that user.
+ * If none are found it sends an error to the client.
+ * Otherwise it sends the found boards information to the client.
+ *
+ * @callback module:boardConnector~getBoardsCallback
+ * @param req - contains the information for the request
+ * @param res - the object to send results to
+ */
--- server/connectors/piece/pieceConnector.js
+++ server/connectors/piece/pieceConnector.js
@@ -8,33 +8,18 @@ let config = require('./piece_connector_config.json');
 const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
+
 //Routing
-
-/**
- * Helper function for post requests.
- *
- * If no ID is specified in the req body it sends an error to the client.
- * If an ID is specified it queries the server to see if a piece with the given ID exists.
- * If so the piece is updated, if not then the piece is created and stored.
- * Then the resulting updated or new piece is sent back to the client.
- * If an error is encountered then the error is sent to the client and execution stops.
- *
- * @param req - contains the information for the request
- * @param res - the object to send results to
- *
- *
- */
-const piecePostHelper = function (req, res) {
+router.post('/api/pieces', function (req, res) {
     let id = req.body.id;
-
+    if(Object.keys(req.body).length === 0){
+        res.statusCode = 400;
+        res.send("No body specified!");
+        return;
+    }
     if(!id){
         res.statusCode = 400;
         res.send("No id specified!");
-        return;
-    }
-    if(Object.keys(req.body).length === 0 ){
-        res.statusCode = 400;
-        res.send("No body specified!");
         return;
     }
     let piece = config.pieces.find((piece) => piece.id === id);
@@ -64,30 +49,14 @@ const piecePostHelper = function (req, res) {
             }
         });
     }
-};
-router.post('/api/pieces', piecePostHelper);
+});
 
-/**
- * Helper function for get requests.
- *
- * If IDs are specified in the req body it queries the server to get the information for each piece with a specified ID.
- * If one is not found then an error is sent to the requesting client and execution stops.
- * Otherwise all the pieces information is sent back to the client.
- * If a creator is specified it queries the server for all pieces created by that user.
- * If none are found it sends an error to the client.
- * Otherwise it sends the found pieces information to the client.
- *
- * @param req - contains the information for the request
- * @param res - the object to send results to
- *
- *
- */
-const pieceGetHelper =function (req, res) {
+router.get('/api/pieces', function (req, res) {
     let ids = req.query.id;
     if(ids) {
         ids = ids.split(',');
     }
-    let creator =  req.query.creator;
+    let creator = req.query.creator;
     if(!ids && !creator){
         res.statusCode = 400;
         res.send("No ids or creator specified!");
@@ -95,7 +64,7 @@ const pieceGetHelper =function (req, res) {
     }
     if(ids && creator){
         res.statusCode = 400;
-        res.send("Both ids and creator specified!");
+        res.send("Both ids and creator specified");
         return;
     }
     let returnPieces = [];
@@ -114,21 +83,51 @@ const pieceGetHelper =function (req, res) {
     }
     if(ids){
         config.pieces.forEach(function (piece) {
-            for(let i=0;i < ids.length;i++) {
+            for(let i = 0; i < ids.length; i++) {
                 if (piece.id == ids[i]) {
                     returnPieces.push(piece);
                     return;
                 }
             }
         });
-        if(returnPieces.length === 0 || returnPieces.length != ids.length){
+        if(returnPieces.length === 0 || returnPieces.length != ids.length) {
             res.statusCode = 404;
             res.send("id/s not found!");
             return;
         }
         res.send(returnPieces);
     }
-};
+});
 
-router.get('/api/pieces', pieceGetHelper);
 module.exports = router;
+
+/** Documentation **/
+
+/**
+ * Helper function for post requests.
+ *
+ * If no ID is specified in the req body it sends an error to the client.
+ * If an ID is specified it queries the server to see if a piece with the given ID exists.
+ * If so the piece is updated, if not then the piece is created and stored.
+ * Then the resulting updated or new piece is sent back to the client.
+ * If an error is encountered then the error is sent to the client and execution stops.
+ *
+ * @callback module:pieceConnector~postPiecesCallback
+ * @param req - contains the information for the request
+ * @param res - the object to send results to
+ */
+
+/**
+ * Helper function for get requests.
+ *
+ * If IDs are specified in the req body it queries the server to get the information for each piece with a specified ID.
+ * If one is not found then an error is sent to the requesting client and execution stops.
+ * Otherwise all the pieces information is sent back to the client.
+ * If a creator is specified it queries the server for all pieces created by that user.
+ * If none are found it sends an error to the client.
+ * Otherwise it sends the found pieces information to the client.
+ *
+ * @callback module:pieceConnector~getPiecesCallback
+ * @param req - contains the information for the request
+ * @param res - the object to send results to
+ */
