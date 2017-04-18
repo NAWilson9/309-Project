b55a1eac40211d64f3b93860ca0f161f117918c0
Nick Wilson <nwilson9@iastate.edu>
2017-04-17 21:08:28 -0500

    Added create user functionality

--- client/js/actions/user_actions.js
+++ client/js/actions/user_actions.js
@@ -35,3 +35,21 @@ export function login(username, password, callback){
         }
     });
 }
+
+export function register(username, password, callback){
+    let credentials = {
+        'username': username,
+        'password': password
+    };
+
+    request({method:'POST', url:'/api/user/register', body:credentials, json: true}, function(err, response, body){
+        if(response.statusCode === 200){
+            store.dispatch({
+                type: 'login',
+                payload: body,
+            });
+        } else if(response.statusCode === 400){
+            callback();
+        }
+    });
+}
