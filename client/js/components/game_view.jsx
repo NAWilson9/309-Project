21f39bbd82dd907a5cefcee1b53ce94332dde6d8
Nickolas.Wilson <Nickolas.Wilson@workiva.com>
2017-04-28 10:21:27 -0500

    g2g

--- client/js/components/game_view.jsx
+++ client/js/components/game_view.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from "react-redux"
-import { Grid } from 'semantic-ui-react'
+import { Grid, Message } from 'semantic-ui-react'
 
 import Board from './board.jsx';
 import BoardSidebar from './board_sidebar.jsx';
@@ -16,25 +16,26 @@ const getStyles = function(){
 
 @connect((store) => {
     return {
-        winner: store.game.over,
+        winner: store.game.winner,
     };
 })
 export default class GameView extends React.Component{
     render() {
         return (
-            <Grid style={getStyles()}>
-                {this.props.winner === true &&
+            <div>
+                {this.props.winner != null &&
                     <Message
                         error
                         content="Game over!"
                         style={{
-                            marginTop: '-75px',
                         }}
                     />
                 }
+            <Grid style={getStyles()}>
                 <Grid.Column width={7}><Board/></Grid.Column>
                 <Grid.Column width={9}><BoardSidebar/></Grid.Column>
             </Grid>
+            </div>
         );
     }
 };
